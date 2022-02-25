e378485487e45413888f62f66dac2841ce6bf9f8
snakesonabrain <bruno@pro-found.be>
2022-02-25 12:14:37 +0100

    Added function for retrieving the ISFOG2020 pile driving dataset

--- mlgeo/datasets.py
+++ mlgeo/datasets.py
@@ -7,6 +7,7 @@ else:
     import pickle
 import urllib.request
 import mlgeo
+import pandas as pd
 
 
 
@@ -36,3 +37,32 @@ def load_slt():
         data = pickle.load(handle)
 
     return data
+
+def load_isfogpiledriving(kind="featurematrix"):
+    """
+    Loads the ISFOG2020 pile driving dataset from the module directory (if previously downloaded)
+    or from the remote repository.
+
+    :param kind: Kind of data to download. If equal to ``"featurematrix"`` the featurematrix with pile dimensions, hammer performance and CPT data is downloaded. If equal to ``"fullcpt"`` the full CPT data is downloaded.
+
+    :returns: Pandas dataframe with the data loaded from csv
+    """
+    if kind == "featurematrix":
+        filename = "all_data_withnormalised.csv"
+    elif kind == "fullcpt":
+        filename = "full_cpt_data_withnormalised.csv"
+    else:
+        raise IOError("Argument kind must be one of 'featurematrix' or 'fullcpt'")
+
+    if not os.path.isfile(os.path.join(mlgeo.dataset_path, filename)):
+        print('-- Downloading dataset... Do not forget to cite authors. --')
+
+        url = 'https://mlgeo-datasets.s3.eu-central-1.amazonaws.com/isfog-piledriving/' + filename
+        urllib.request.urlretrieve(
+            url, os.path.join(mlgeo.dataset_path, filename))
+
+        print('-- Done --')
+
+        data = pd.read_csv(os.path.join(mlgeo.dataset_path, filename))
+
+        return data
--- setup.py
+++ setup.py
@@ -22,6 +22,7 @@ setuptools.setup(
     python_requires='>=3.7',
     install_requires=[
         'numpy>=1.19',
-        'pickle5'
+        'pickle5',
+        'pandas'
     ]
 )
